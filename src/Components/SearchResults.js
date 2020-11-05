dc79622788d475cf33b74e143c5f417b1fba20e1
prnvbirajdar <prnvbirajdar@gmail.com>
2020-11-05 03:30:46 -0500

    added movie images

--- src/Components/SearchResults.js
+++ src/Components/SearchResults.js
@@ -2,10 +2,14 @@ import React from 'react';
 
 const SearchResults = (props) => {
 
+    const img_api = "https://image.tmdb.org/t/p/w342"
+
     const searchedMovies = props.searchList.map((movie)=>{
-        return (<div>
-            <p>{movie.title}</p>
+        return (<div key={movie.id}>
+            <h3>{movie.title}</h3>
+            <img src={img_api + movie.poster_path} alt="poster"/>
             <p>{movie.vote_average}</p>
+            <p>{movie.overview}</p>
         </div>)
     })
 
