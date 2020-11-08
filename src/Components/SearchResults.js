83e36d5c3b1d277fc55d608f70b818f58ce23a2c
prnvbirajdar <prnvbirajdar@gmail.com>
2020-11-07 21:16:46 -0500

    refactoring to use react hooks

--- src/Components/SearchResults.js
+++ src/Components/SearchResults.js
@@ -1,23 +1,23 @@
-import React from 'react';
+// import React from 'react';
 
-const img_api = "https://image.tmdb.org/t/p/w342"
+// const img_api = "https://image.tmdb.org/t/p/w342"
 
-const SearchResults = (props) => {
+// const SearchResults = (props) => {
 
-    const searchedMovies = props.searchList.map((movie)=>{
-        return (<div key={movie.id}>
-            <h3>{movie.title}</h3>
-            <img src={img_api + movie.poster_path} alt="poster"/>
-            <p>{movie.vote_average}</p>
-            <p>{movie.overview}</p>
-        </div>)
-    })
+//     const searchedMovies = props.searchList.map((movie)=>{
+//         return (<div key={movie.id}>
+//             <h3>{movie.title}</h3>
+//             <img src={img_api + movie.poster_path} alt="poster"/>
+//             <p>{movie.vote_average}</p>
+//             <p>{movie.overview}</p>
+//         </div>)
+//     })
 
-    return (
-        <div>
-            {searchedMovies}
-        </div>  
-    );
-}
+//     return (
+//         <div>
+//             {searchedMovies}
+//         </div>  
+//     );
+// }
  
-export default SearchResults;
+// export default SearchResults;
