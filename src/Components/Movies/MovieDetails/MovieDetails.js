0c5f0daf1e76dfa35cf9721f95be545dfa38f117
prnvbirajdar <prnvbirajdar@gmail.com>
2020-11-22 22:52:25 -0500

    basic movieDetails styling

--- src/Components/Movies/MovieDetails/MovieDetails.js
+++ src/Components/Movies/MovieDetails/MovieDetails.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import {instance, img_api} from '../../../Api/axios';
-
+import './MovieDetails.css'
 
 function MovieDetails({match}) {
     const [movie,setMovie] = useState([])
@@ -25,29 +25,36 @@ function MovieDetails({match}) {
 
 
     return (
-
-        <div>
-            <img src={img_api.poster + movie?.poster_path} alt={movie?.title}/>
-            <h1>{movie?.title || movie?.original_name || movie?.name}</h1>
-            <p>{movie.overview}</p> 
-            <p>{movie.runtime}</p>
-            <p>{movie.release_date.substring(0,4)}</p>   {/*only shows the year*/}
-            <p>{movie.vote_average}</p>
-            {movie.genres.map(m=>{
-                return <p key={m.id}>{m.name}</p>
-            })}
-
-            {credits.cast.slice(0,5).map(credit=>{
-                return <div key={credit.id}> {/*maps over cast array and renders first 5 actors*/}
-                    <p>{credit?.name || credit?.original_name}</p>
-                    <p>{credit?.character}</p>
-                    <img src={img_api.poster + credit?.profile_path} alt={credit?.name}/>
+        <div className="movieDetails">
+            <div className="movieDetails__main">
+                <img className="movieDetails__poster" src={img_api.poster + movie?.poster_path} alt={movie?.title}/>
+                <div className="movieDetails__info">
+                    <h1>{movie?.title || movie?.original_name || movie?.name}</h1>
+                    <p>{movie.overview}</p> 
+                    {credits.crew
+                        .filter(credit=> credit.job === 'Director') 
+                        .map(credit=> <p key={credit.id}>{credit.original_name}</p>)} {/*filter over the crew array to find the director and then render it*/}
+                    <p>{movie.runtime}</p>
+                    <p>{movie.release_date.substring(0,4)}</p>   {/*only shows the year*/}
+                    <p>{movie.vote_average}</p>
+                    {movie.genres.map(m=>{
+                        return <p key={m.id}>{m.name}</p>
+                    })}
                 </div>
-            })}
+            </div>
 
-            {credits.crew
-                .filter(credit=> credit.job === 'Director') 
-                .map(credit=> <p key={credit.id}>{credit.original_name}</p>)} {/*filter over the crew array to find the director and then render it*/}
+            <div className="movieDetails__cast">
+                {credits.cast.slice(0,5).map(credit=>{
+                    return (
+                        <div key={credit.id}> {/*maps over cast array and renders first 5 actors*/}
+                            <img src={img_api.poster + credit?.profile_path} alt={credit?.name}/>
+
+                            <p>{credit?.name || credit?.original_name}</p>
+                            <p>{credit?.character}</p>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
 
        
