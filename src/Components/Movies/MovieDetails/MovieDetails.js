db16d0c585e125aeb81a2b97d5b8b028f4f03fe4
prnvbirajdar <prnvbirajdar@gmail.com>
2020-11-23 03:46:17 -0500

    added similar movies and removed broken images

--- src/Components/Movies/MovieDetails/MovieDetails.js
+++ src/Components/Movies/MovieDetails/MovieDetails.js
@@ -1,28 +1,37 @@
 import React, {useState, useEffect} from 'react'
 import {instance, img_api} from '../../../Api/axios';
 import './MovieDetails.css'
+import {Link} from 'react-router-dom'
 
 function MovieDetails({match}) {
     const [movie,setMovie] = useState([])
     const [credits, setCredits] = useState([])
+    const [similar, setSimilar] = useState([])
 
     useEffect(()=>{
         const fetchMovie = async ()=>{
             const response = await instance.get(`/movie/${match.params.id}`)   
             const responseCredits = await instance.get(`/movie/${match.params.id}/credits`)
+            const responseSimilar = await instance.get(`/movie/${match.params.id}/similar`)
+
+                // refinedSimilarMovies removes all the movie objects with broken image files
+                // and slices the array to render 10 working movie objects
+                const refinedSimilarMovies = responseSimilar.data.results.filter((movie)=>{
+                    return movie?.poster_path !== null || "" || undefined
+                }).slice(0,4)
+
             setMovie(response.data)
             setCredits(responseCredits.data)
-            console.log(responseCredits.data.cast);
-            console.log(responseCredits.data.crew);
+            setSimilar(refinedSimilarMovies)
 
-            return response
+            return {response, responseCredits, responseSimilar}
         }
         fetchMovie()
     
     },[match.params.id])
 
+    //prevents from rendering empty arrays and giving errors.
     if(!movie.genres || !credits.cast || !credits.crew) return null
-
 
     return (
         <div className="movieDetails">
@@ -30,34 +39,51 @@ function MovieDetails({match}) {
                 <img className="movieDetails__poster" src={img_api.poster + movie?.poster_path} alt={movie?.title}/>
                 <div className="movieDetails__info">
                     <h1>{movie?.title || movie?.original_name || movie?.name}</h1>
-                    <p>{movie.overview}</p> 
+                    <p>Overview: {movie.overview}</p> 
                     {credits.crew
                         .filter(credit=> credit.job === 'Director') 
-                        .map(credit=> <p key={credit.id}>{credit.original_name}</p>)} {/*filter over the crew array to find the director and then render it*/}
-                    <p>{movie.runtime}</p>
-                    <p>{movie.release_date.substring(0,4)}</p>   {/*only shows the year*/}
+                        .map(credit=> <p key={credit.id}>Director: {credit.original_name}</p>)} {/*filter over the crew array to find the director and then render it*/}
+                    <p>Runtime: {Math.floor(movie.runtime/60)}hr {(movie.runtime%60)}min </p> {/*converts mins to hr min*/}
+                    <p>Release year: {movie.release_date.substring(0,4)}</p>   {/*only shows the year*/}
                     <p>{movie.vote_average}</p>
-                    {movie.genres.map(m=>{
-                        return <p key={m.id}>{m.name}</p>
-                    })}
+                    <div className="movieDetails__genres">
+                        {movie.genres.map(m=>{
+                            return <p key={m.id} className="movieDetails__genre">{m.name}</p>
+                        })}
+                    </div>
                 </div>
             </div>
 
             <div className="movieDetails__cast">
-                {credits.cast.slice(0,5).map(credit=>{
+                {credits.cast
+                    .filter(credit=>{return credit?.profile_path !== null || "" || undefined})
+                    .slice(0,4)
+                    .map(credit=>{
                     return (
-                        <div key={credit.id}> {/*maps over cast array and renders first 5 actors*/}
+                        <div key={credit.id}> {/*maps over cast array and renders first 4 actors*/}
                             <img src={img_api.poster + credit?.profile_path} alt={credit?.name}/>
-
                             <p>{credit?.name || credit?.original_name}</p>
                             <p>{credit?.character}</p>
                         </div>
                     )
                 })}
             </div>
-        </div>
 
-       
+            <div className='movieDetails__similar'>
+                {similar.map(movie=>{
+                return (
+                    <div key={movie.id}>
+                        <Link to={`/movie/${movie.id}`}><img src={img_api.poster + movie?.poster_path} alt={movie.title}/></Link>
+                        {/*<div className="row__movie-info">
+                            <h1>{movie?.title || movie?.original_name || movie?.name}</h1>
+                            <span>{movie.vote_average}</span>
+                        </div>
+                        <p>{movie.overview}</p> */}
+                    </div>
+                    )  
+                })}
+            </div>
+        </div>
     )
 }
 
