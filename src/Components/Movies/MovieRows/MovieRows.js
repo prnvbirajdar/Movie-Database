5a3cc5bc9a330e01d3a3085ce90f77f2068cd104
prnvbirajdar <prnvbirajdar@gmail.com>
2021-03-01 12:43:12 -0500

    slick removed

--- src/Components/Movies/MovieRows/MovieRows.js
+++ src/Components/Movies/MovieRows/MovieRows.js
@@ -3,6 +3,9 @@ import { instance, img_api } from "../../../Api/axios";
 import "./MovieRows.css";
 import { Link } from "react-router-dom";
 
+// import Slider from "react-slick";
+
+
 const MoviesRow = ({ title, handleAPI, setSearchTerm }) => {
   const [movies, setMovies] = useState([]);
 
@@ -27,30 +30,58 @@ const MoviesRow = ({ title, handleAPI, setSearchTerm }) => {
     fetchMovies();
   }, [handleAPI]);
 
+  // const settings = {
+  //   dots: true,
+  //   infinite: true,
+  //   speed: 500,
+  //   slidesToShow: 7,
+  //   slidesToScroll: 3,
+  // };
+
+
+  // function ScrollBox({ children }) {
+  //   const scrollWrapperRef = useRef();
+  
+  //   const scrollWrapperCurrent = scrollWrapperRef.current;
+  //   useEffect(() => {
+  //     if (scrollWrapperRef.current) {
+  //       const handleDragStart = () => {};
+  //       const handleDragMove = () => {};
+  //       const handleDragEnd = () => {};
+  
+  //       if (scrollWrapperRef.current.ontouchstart === undefined) {
+  //         scrollWrapperRef.current.onmousedown = handleDragStart;
+  //         scrollWrapperRef.current.onmousemove = handleDragMove;
+  //         scrollWrapperRef.current.onmouseup = handleDragEnd;
+  //         scrollWrapperRef.current.onmouseleave = handleDragEnd;
+  //       }
+  //     }
+  //   }, [scrollWrapperCurrent]);
+
   return (
     <React.Fragment>
       <div className="row">
         <h2 className="row__title">{title}</h2>
-        <div className="row__posters">
-          {movies &&
-            movies.map((movie) => {
-              return (
-                <div
-                  key={movie.id}
-                  className="row__movie"
-                  onClick={() => setSearchTerm("")}
-                >
-                  <Link to={`/movie/${movie.id}`}>
-                    <img
-                      src={img_api.poster + movie?.poster_path}
-                      alt={movie.title}
-                      className="row__poster"
-                    />
-                  </Link>
-                </div>
-              );
-            })}
-        </div>
+          <div className="row__posters">
+            {movies &&
+              movies.map((movie) => {
+                return (
+                  <div
+                    key={movie.id}
+                    className="row__movie"
+                    onClick={() => setSearchTerm("")}
+                  >
+                    <Link to={`/movie/${movie.id}`}>
+                      <img
+                        src={img_api.poster + movie?.poster_path}
+                        alt={movie.title}
+                        className="row__poster"
+                      />
+                    </Link>
+                  </div>
+                );
+              })}
+          </div>{" "}
       </div>
     </React.Fragment>
   );
